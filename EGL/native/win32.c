991ed3568361e2c621ef1882e1f225b22bbe4b1c
matanui159 <josh@minteronline.com>
2018-10-08 12:06:55 +1000

    Improved native win32 init

--- EGL/native/win32.c
+++ EGL/native/win32.c
@@ -8,11 +8,23 @@ void tglc_native_init() {
 
 	#if _WIN32_WINNT >= 0x0500
 		CONSOLE_FONT_INFOEX font;
+		font.cbSize = sizeof(CONSOLE_FONT_INFOEX);
 		GetCurrentConsoleFontEx(g_console, false, &font);
 		font.dwFontSize.X = 8;
 		font.dwFontSize.Y = 8;
 		SetCurrentConsoleFontEx(g_console, false, &font);
 	#endif
+
+	CONSOLE_CURSOR_INFO cursor;
+	GetConsoleCursorInfo(g_console, &cursor);
+	cursor.bVisible = false;
+	SetConsoleCursorInfo(g_console, &cursor);
+
+	CONSOLE_SCREEN_BUFFER_INFO buffer;
+	GetConsoleScreenBufferInfo(g_console, &buffer);
+	buffer.dwSize.X = buffer.srWindow.Right - buffer.srWindow.Left + 1;
+	buffer.dwSize.Y = buffer.srWindow.Bottom - buffer.srWindow.Top + 1;
+	SetConsoleScreenBufferSize(g_console, buffer.dwSize);
 }
 
 void tglc_native_exit() {
